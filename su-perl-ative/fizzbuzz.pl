d5b0034e1f8c43f93819a68456f599f7075c341f
JosephBingham <joebingham@me.com>
2017-11-04 23:47:14 -0500

    more bad puns to come

---
+++ su-perl-ative/fizzbuzz.pl
@@ -0,0 +1,16 @@
+use 5.010;
+
+for($i = 3; $i < 1000; $i++){
+    if($i % 15 == 0){
+	say "fizzbuzz";
+    }
+    elsif($i % 5 == 0){
+	say "buzz";
+    }
+    elsif($i % 3 == 0){
+	say "fizz";
+    }
+    else{
+	say $i;
+    }	
+}
