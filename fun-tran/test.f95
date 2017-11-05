7d2348845bb42c274693231ded752a17e4edc1ad
JosephBingham <joebingham@me.com>
2017-11-04 23:28:20 -0500

    more bad puns to come

---
+++ fun-tran/test.f95
@@ -0,0 +1,14 @@
+PROGRAM test
+  INTEGER :: I 
+  DO I = 3, 1000
+     IF(MOD(I,15) == 0) THEN
+        PRINT *, "FIZZBUZZ"
+     ELSE IF(MOD(I, 5) == 0) THEN
+        PRINT *, "BUZZ"
+     ELSE IF(MOD(I, 3) == 0) THEN
+        PRINT *, "FIZZ"
+     ELSE
+        PRINT *, I
+     END IF
+  END DO
+END PROGRAM test
